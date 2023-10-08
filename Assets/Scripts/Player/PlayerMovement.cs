c24475d07407a3422f05e1cc622db5ad0800ef19
MrcoAlba <mrcoalba@gmail.com>
2023-10-07 21:25:02 -0500

    Animator for walking done

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Vector2 speed = new (4f, 4f);
+    // VARIABLES ---------------------------------------------------------------
+    [SerializeField] private Vector3 speed = new (4f, 0f, 4f); // new (X, Y, Z)
     private Rigidbody rb;
+    private Animator animator;
     private Vector2 moveDir;
+    // VARIABLES ---------------------------------------------------------------
+
+
 
     // Reference components from this script
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Reference components from other script
@@ -26,14 +32,16 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector3(
             moveDir.x * speed.x,// This is the X axis
-            0f,                 // This is the Y axis
-            moveDir.y * speed.y // This is the Z axis
+            0f        * speed.y,// This is the Y axis
+            moveDir.y * speed.z // This is the Z axis
         );
     }
 
     private void OnMovement(InputValue value)
     {
         moveDir = value.Get<Vector2>();
+        animator.SetFloat("Horizontal", moveDir.x);
+        animator.SetFloat("Vertical", moveDir.y);
     }
 
 }
