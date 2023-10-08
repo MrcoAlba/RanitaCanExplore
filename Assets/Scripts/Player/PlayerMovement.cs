f460a9c83d8526b6f5f9ac58bce102b44d2702d4
MrcoAlba <mrcoalba@gmail.com>
2023-10-07 22:57:06 -0500

    Idle movement done and colliders to building

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // VARIABLES ---------------------------------------------------------------
-    [SerializeField] private Vector3 speed = new (4f, 0f, 4f); // new (X, Y, Z)
+    [SerializeField] private Vector3 speed = new(2f, 0f, 2f); // new (X, Y, Z)
     private Rigidbody rb;
     private Animator animator;
     private Vector2 moveDir;
@@ -30,9 +30,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         rb.velocity = new Vector3(
             moveDir.x * speed.x,// This is the X axis
-            0f        * speed.y,// This is the Y axis
+            0f * speed.y,// This is the Y axis
             moveDir.y * speed.z // This is the Z axis
         );
     }
@@ -40,8 +42,25 @@ public class PlayerMovement : MonoBehaviour
     private void OnMovement(InputValue value)
     {
         moveDir = value.Get<Vector2>();
-        animator.SetFloat("Horizontal", moveDir.x);
-        animator.SetFloat("Vertical", moveDir.y);
+        // if moveDir isn't normalize, when you move up and right,
+        // the velocity will be 1 for Y and 1 for X.
+        moveDir.Normalize();
+        // Animations for "walking" or "idling"
+        if (Mathf.Abs(moveDir.x) > Mathf.Epsilon ||
+            Mathf.Abs(moveDir.y) > Mathf.Epsilon)
+        {
+            animator.SetBool("IsWalking", true);
+            animator.SetFloat("Horizontal", moveDir.x);
+            animator.SetFloat("Vertical", moveDir.y);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+            // Horizontal and Vertical aren't changed here bc we 
+            // want to know where was the last direction the player
+            // was going to set the idle direction animation
+        }
+
     }
 
 }
